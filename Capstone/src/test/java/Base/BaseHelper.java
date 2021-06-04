2f5702e3c3beddd22763150b9fe8fadcf36dab5a
dell1 <dell1@Harmanjeet>
2021-06-03 22:33:18 -0400

    ,properties file added

--- Capstone/src/test/java/Base/BaseHelper.java
+++ Capstone/src/test/java/Base/BaseHelper.java
@@ -1,16 +1,83 @@
 package Base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
-import Config.Capstone.Config;
+import Config.ReadPropertyFile;
 
-public class BaseHelper extends Config{
+public class BaseHelper extends ReadPropertyFile {
 	
-	public void getobject  (WebDriver driver ,String a) {
-		driver.get(prop.getProperty(a));
-		
-		
+    public static WebDriver driver;
+	
+	
+	public static WebDriver startApp (String url, String browserName) throws Exception
+    {
+        //if(browserName.equals("Chrome"))
+        //{
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+            
+            driver = new ChromeDriver();
+
+        //}
+        //else if(browserName.equals("Firefox"))
+        //{
+          //  System.setProperty(getObject("firefoxDriver"), getObject("firefoxDriver_Location"));
+            //driver = new FirefoxDriver();
+        //}
+        //else if(browserName.equals("Edge"))
+        //{
+          //  System.setProperty(getObject("edgeBrowser"), getObject("edgeBrowser_Location"));
+            //driver = new InternetExplorerDriver();
+        //}
+        //else
+        //{
+          //  System.out.println("Browser not supported");
+        //}
+        //driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get(url);
+       System.out.println("Abc  :" + getObject("abc"));
+       System.out.println("Home_Page_Visit  :" + getObject("home_Page_Visit"));
+        return driver;
+    }
+
+	public static void loginToHomePage( String url) throws Exception
+	{   
+	    driver.get(getObject("url"));
+	    
+	    //Old login with Dropdown
+	    //LoginPage.selectDropDown(driver);
+	    
+	    //New login with HQ Upgrade .net version 2
+	    //Thread.sleep(2000);
+	   // LoginPage.clickMedworxxSignInButton(driver);
+	    
+	    Thread.sleep(1000);
+		//LoginPage.enterUsername(driver,userId);
+		//LoginPage.enterPassword(driver,pass);
+		//LoginPage.clickLogin(driver);
 	}
-	
 
+	//Generic Method for getting text 
+ 	 public static String getTextBase( By by)
+ 	 {
+ 		return(driver.findElement( by).getText());
+ 	 }
+ 	 
+ 	//Generic Method for click 
+ 	 public static  void click(By by)
+ 	 {
+ 		
+		driver.findElement( by).click();
+ 	 }
+
+ 	 
+ 	 
+ 	 
 }
+
+
+
---
+++ Capstone/src/test/java/Config/ReadPropertyFile.java
@@ -0,0 +1,31 @@
+
+
+
+package Config;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ReadPropertyFile {
+static Properties properties;
+	
+	public static void loadData() throws Exception
+	{
+		properties = new Properties();
+		File f= new File("C:\\Users\\dell1\\eclipse-workspace\\Capstone\\src\\test\\java\\data.properties");
+			FileReader obj = new FileReader(f);
+		properties.load(obj);
+	}
+	
+	public static String getObject(String Data) throws Exception
+	{
+		loadData();
+		String data = properties.getProperty(Data);
+		return data;
+	}
+
+}
